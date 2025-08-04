a027a1860a2b31969aa569c7b5625ee1fa141bfd
quocdz10 <anhquoc10.tvt@gmail.com>
2025-08-04 13:23:26 +0700

    getProfile+editProfile

---
+++ client/my-app/src/app/actions/gettoken.action.js
@@ -0,0 +1,17 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { client } from "../helpers/fetch_api/client";
+
+export async function getToken() {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("access_token");
+  const refreshToken = cookieStore.get("refresh_token");
+  console.log("accessToken: ", accessToken);
+  console.log("refreshToken: ", refreshToken);
+  const exp = await client.post("/auth/get-expires-token", {
+    access_token: accessToken.value,
+    refresh_token: refreshToken.value,
+  });
+  return { accessToken, refreshToken, exp: exp.data };
+}
